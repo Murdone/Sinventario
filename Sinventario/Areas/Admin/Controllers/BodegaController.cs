6330738ef10803d61556ce7a08156965db8049ef
vasqu <vasqu@DESKTOP-4U0SRH3>
2022-11-08 15:04:59 -0300

    controladorechoa manoaun nio terminado

---
+++ Sinventario/Areas/Admin/Controllers/BodegaController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SInventario.AccesoDatos.Repositorio.IRepositorio;
+
+namespace Sinventario.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class BodegaController : Controller
+    {
+        private readonly IUnidadTrabajo _unidadTrabajo; //unidad de trabajo
+        public BodegaController(IUnidadTrabajo unidadTrabajo)
+        {
+            _unidadTrabajo = unidadTrabajo;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        #region Api
+        [HttpGet]
+        public IActionResult ObtenerTodos()
+        {
+            var todos = _unidadTrabajo.Bodega.ObtenerTodos();
+            return Json(new {data = _unidadTrabajo});
+        }
+        #endregion
+    }
+}
